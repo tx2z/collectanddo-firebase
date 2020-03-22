f06663fb12ef141c6c74f7cb6e853b3d9f052bfd
tx2z <tx2z@caosmental.com>
2020-03-22 11:04:22 +0100

    Added appflow sdk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDinNOC3UB8tnMuqDBohdSMECAnHgUJ_OU',
+    authDomain: 'collectanddo.firebaseapp.com',
+    databaseURL: 'https://collectanddo.firebaseio.com',
+    projectId: 'collectanddo',
+    storageBucket: 'collectanddo.appspot.com',
+    messagingSenderId: '1051374144941',
+    appId: '1:1051374144941:web:3773bcae11484b5b1b6524',
+    measurementId: 'G-JEW7NH2S69'
+  }
 };
